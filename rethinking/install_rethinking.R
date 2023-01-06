010e239b5728ceb18769dce7faf5f32444668476
oskarryden <rydenoskar@gmail.com>
2023-01-06 14:41:07 +0100

    rethinknig

---
+++ rethinking/install_rethinking.R
@@ -0,0 +1,5 @@
+# installera rethinking
+# Länk: https://github.com/rmcelreath/rethinking
+
+install.packages(c("coda","mvtnorm","devtools","loo","dagitty","shape"))
+devtools::install_github("rmcelreath/rethinking")
