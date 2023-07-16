ddc48de031c5f5b4aa6165c4623843a9728a6457
oskarryden <rydenoskar@gmail.com>
2023-07-16 15:10:57 +0200

    cpp

---
+++ cpp/basic.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // Declare
+    int a, b;
+    int result;
+
+    // Define
+    a = 5;
+    b = 2;
+    a = a + 1;
+    result = a - b;
+
+    // Print
+    cout << result << endl;
+    return 0;
+}
